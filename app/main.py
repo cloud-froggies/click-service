83e114b93c659e82a957e905f7ca62f168b8e0c8
elguetajj <elguetaj@ufm.edu>
2021-09-28 08:50:04 -0600

    adds dynamodb sessions

--- app/main.py
+++ app/main.py
@@ -5,16 +5,19 @@ from fastapi.logger import logger
 from fastapi.param_functions import Query
 from fastapi.responses import RedirectResponse
 
-
 import  pymysql
 import os
 import logging 
 
-DB_ENDPOINT = os.environ.get('db_endpoint')
-DB_ADMIN_USER = os.environ.get('db_admin_user')
-DB_ADMIN_PASSWORD = os.environ.get('db_admin_password')
-DB_NAME = os.environ.get('db_name')
+import boto3
 
+# DB_ENDPOINT = os.environ.get('db_endpoint')
+# DB_ADMIN_USER = os.environ.get('db_admin_user')
+# DB_ADMIN_PASSWORD = os.environ.get('db_admin_password')
+# DB_NAME = os.environ.get('db_name')
+
+# AWS_ACCESS_KEY_ID =''
+# AWS_SECRET_ACCESS_KEY=''
 
 app = FastAPI(title='Matching Service',version='0.1')
 gunicorn_logger = logging.getLogger('gunicorn.error')
@@ -26,23 +29,30 @@ if __name__ != "main":
 else:
     logger.setLevel(logging.DEBUG)
 
-
-def get_db_conn():
-    try:
-        conn = pymysql.connect(host=DB_ENDPOINT, user=DB_ADMIN_USER, passwd=DB_ADMIN_PASSWORD, db=DB_NAME, connect_timeout=5)
-        return conn
-    except pymysql.MySQLError as e:
-        logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
-        logger.error(e)
-        raise
-
-
-
 @app.get("/")
 def read_root():
     return {"Service": "click"}
 
+@app.get("/table")
+def read_root():
+    client = boto3.client('dynamodb')
+
+    response = client.describe_table(
+        TableName='sessions'
+    )
+
+    return {"Service": response}
+
 
 @app.get("/click",response_class=RedirectResponse, status_code=302)
-async def click(link:str):
-    return link
+async def click(query_id:str,impression_id:str):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
+    table = dynamodb.Table('sessions')
+    response = table.get_item(
+        Key={
+            'query_id': query_id,
+            'impression_id': impression_id
+        }
+    )
+    item = response['Item']['advertiser_url']
+    return item
